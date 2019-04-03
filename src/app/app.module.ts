744e86959a14aac6f6c0b5117d423dbcdb1d68b1
Maria Luiza Fornagieri <mluiza_fornagieri@outlook.com>
2019-04-03 16:33:07 -0300

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,10 +10,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQeCe05A-jGCnHbAdi5UrzeNllI_uNS3M',
+  authDomain: 'cozinhando-em-casa-77710.firebaseapp.com',
+  databaseURL: 'https://cozinhando-em-casa-77710.firebaseio.com',
+  projectId: 'cozinhando-em-casa-77710',
+  storageBucket: '',
+  messagingSenderId: '142420901588'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
   providers: [
     StatusBar,
     SplashScreen,
